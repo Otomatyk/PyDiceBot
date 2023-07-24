99c174d6f9db67f7d37c8f4820dba905b53eeb44
Otomatyk <123239873+Otomatyk@users.noreply.github.com>
2023-07-24 16:37:07 +0200

    Update dice_engine_core.py

--- dice_engine_core.py
+++ dice_engine_core.py
@@ -1,55 +1,84 @@
-import re
-from random import randint, choices, shuffle
+"""
+Coeur de DiceIO, définit les fonctions 'exec_cmd' et 'exec_commande'.
+"""
+
+import sys
+from functools import lru_cache, partial
+from random import choices, randint, seed, shuffle
 from re import search, split, match
-from dice_engine_l_et_u import exec_l, exec_u
-from dice_engine_macro import exec_cmd_macro
+from time import time
 
-RE_FORMAT_TOKEN_VALIDE = re.compile("[0-9]*(d|e)[0-9]+.*")
-RE_N_DES = re.compile("([0-9]+)(?=(d|e))")
-RE_N_FACE = re.compile("(?<=(d|e))[0-9]+")
+from diceio_choice import exec_l, exec_u
+from diceio_macro import exec_cmd_macro
 
-RE_KEEP = re.compile("(?<=k)(|\-)[0-9]+")
 
-RE_MAP = re.compile("(g|r|a|b)\(.[0-9]+(?=\))")
+LancerDes = list[int]
+Condition = str #">" | "<" | "!=" | "=="
+Somme = int
 
-RE_OPS = re.compile("(?<!k)[\+\-]")
-RE_SPLIT_TOKEN = re.compile("(\+|(?<!k)\-)")
+MAX_MESSAGE_LEN: int = 2000
+IGNORED_COMMANDS: frozenset = frozenset(("!levels", "!rank"))
 
-RE_FORMAT_U = re.compile("[0-9]+u.+")
-RE_FORMAT_L = re.compile("[0-9]+l.+")
+sr_users: set = set()
+last_sr_dices: dict = {}
 
-MAX_MESSAGE_LEN = 2000
+def relancer_last_sr(user_id: int) -> str:
+    n_des = last_sr_dices.get(user_id)
+    if n_des == 0:
+        return '# Aucune dès à relancer !'
+    return f"# {n_des} dès rélancés \n"+exec_lancer_des_sr(str(n_des), user_id)
 
-Lancer_des = list[int]
-Condition = str #">" | "<" | ">=" | "<=" | "!=" | "=="
+def exec_lancer_des_sr(cmd: str, user_id: int) -> str | tuple[Somme, LancerDes]:
+    "Recoie la commande nettoyée et renvoie le message à afficher ou le resultat des dès"
 
-def exec_lancer_des(cmd:str) -> tuple[int, Lancer_des]: 
+    if cmd.isdigit():
+        n_des = int(cmd)
+    
+    elif match("[0-9]+(?=d6$)", cmd):
+        n_des = int(search("[0-9]+(?=d)", cmd).group())
+
+    else:
+        return exec_lancer_des(cmd)
+
+    result = choices(get_dice_range(6), k=n_des)
+
+    n_6 = result.count(6)
+    n_succes = result.count(5) + n_6
+    n_1 = result.count(1)
+
+    last_sr_dices[user_id] = n_des-n_succes
+
+    return f'Nombre de succes: {n_succes}\
+        \nNombre de 6 : {n_6}\
+        \nNombre de 1 : {n_1}'
+
+def exec_lancer_des(cmd: str) -> tuple[Somme, LancerDes]:
     "Recoie la commande nettoyée"
-    cmd = "+"+cmd #Le premier lancé de dès rajouter forcement sa somme
+    cmd = "+"+cmd #Le premier token rajoute forcement sa somme
 
-    #D'abbord diviser les tokens et trouver l'ordes des opérateurs
-    liste_token = split(RE_SPLIT_TOKEN, cmd)[1:]
+    # D'abbord diviser les tokens et trouver l'ordes des opérateurs
+    liste_token = split("(\+|(?<!k)\-)", cmd)[1:]
 
-    #Puis les éxécuter
+    # Puis les éxécuter
     lancer_des = []
     somme_final = 0
 
-    for op, token in zip(liste_token[::2], liste_token[1::2]):
-        
+    for op_arithmeique, token in zip(liste_token[::2], liste_token[1::2]):
+
         assert bool(token), "Token manquant, trop d'opérateurs arithmétiques"
-        
+
         if token.isdigit():
             somme = int(token)
             lancer_des.append(f"_{somme}")
         else:
             somme, lance = exec_token(token)
             lancer_des.extend(lance)
-        
-        somme_final = somme_final+somme if op == "+" else somme_final-somme
-        
+
+        somme_final += somme if op_arithmeique == "+" else -somme
+
     return somme_final, lancer_des
 
-def exec_token(token:str) -> tuple[int, Lancer_des]:
+def exec_token(token: str) -> tuple[Somme, LancerDes]:
     """Execute un token (les tokens sont séparés de + et de - dans les lancer de dès)
 
     Args:
@@ -58,14 +87,15 @@ def exec_token(token:str) -> tuple[int, Lancer_des]:
     Returns:
        tuple[int, list[int]]: La somme du résultat et le lancé de dès
     """
-    n_des = search(RE_N_DES, token)
+
+    n_des = search("([0-9]+)(?=(d|e))", token)
     n_des = int(n_des.group()) if n_des else 1
 
-    n_face = search(RE_N_FACE, token)
-    assert hasattr(n_face, "group"), "Syntaxe invalide, ll manque le nombre de faces"
+    n_face = search("(?<=(d|e))[0-9]+", token)
+    assert n_face is not None, "Syntaxe invalide, ll manque le nombre de faces"
     n_face = int(n_face.group())
 
-    lancer_des = choices(range(1, n_face+1), k=n_des)
+    lancer_des = choices(range(1, n_face), k=n_des)
 
     #Explode
     if "e" in token:
@@ -76,15 +106,16 @@ def exec_token(token:str) -> tuple[int, Lancer_des]:
 
     #Keep
     if "k" in token:
-        keep = search(RE_KEEP, token)
-        if keep:
+        if keep := search("(?<=k)(|\-)[0-9]+", token):
             keep = int(keep.group())
+            assert abs(keep) != 0, "Utiliser Keep néssecite que le nombre de dès à garder soit non-nulle"
             lancer_des = appliquer_keep(lancer_des, keep)
+        else:
+            raise AssertionError("Il manque le nombre de dès de garder avec le keep")
 
     #Map
     if "(" in token:
-        map_ = search(RE_MAP, token)
-        if map_:  
+        if map_ := search("(g|r|a|b)\(.[0-9]+(?=\))", token):
             map_ = map_.group()
             lancer_des = appliquer_map(lancer_des, map_[0], map_[2], int(map_[3:]), n_face+1)
 
@@ -94,26 +125,27 @@ def exec_token(token:str) -> tuple[int, Lancer_des]:
 
     return sum(lancer_des), lancer_des
 
-def appliquer_keep(lancer_des:Lancer_des, keep:int) -> Lancer_des:
+def appliquer_keep(lancer_des: LancerDes, keep: int) -> LancerDes:
     """Garde les meilleurs (+) ou les pires résultats (-) d'un lancé de dès, mélange le lancé après
 
     Args:
-        lancer_des (list[int]): Le lancé de dès
+        LancerDes (list[int]): Le lancé de dès
         keep (signed int): Les dès à garder. Avec un nombre négatif, garde les pires résultats
 
     Returns:
         list[int]: Le lancé de dès après keep
     """
-    if keep > 0:
-        lancer_des_trier = sorted(lancer_des, reverse=True)
-    else:
-        lancer_des_trier = sorted(lancer_des)
+
+    if len(lancer_des) < abs(keep):
+        raise AssertionError("Erreur : Le nombre de dès à garder est supérieur au nombre de dès")
+
+    lancer_des_trier = sorted(lancer_des, reverse=keep > 0)
 
     lancer_des = lancer_des_trier[0:abs(keep)]
     shuffle(lancer_des)
     return lancer_des
 
-def eval_condition_map(des:int, condition:Condition, n_condition:int) -> bool:
+def _eval_condition(des: int, condition: Condition, n_condition: int) -> bool:
     """Test si un dès respècte une condition
 
     Args:
@@ -124,9 +156,20 @@ def eval_condition_map(des:int, condition:Condition, n_condition:int) -> bool:
     Returns:
         bool: Le résultat du test
     """
-    return eval(str(des) +condition+ str(n_condition) )
-                
-def relancer(des:int ,condition:Condition, n_condition:int, n_face:int) -> int:
+
+    match condition:
+        case "=":
+            return des == n_condition
+        case "!":
+            return des != n_condition
+        case "<":
+            return des < n_condition
+        case ">":
+            return des > n_condition
+
+    raise ValueError("Valeur invalide pour 'condition'")
+
+def relancer(des: int ,condition: Condition, n_condition: int, n_face: int) -> int:
     """Relance le dès si la condition est vraie
 
     Args:
@@ -139,38 +182,34 @@ def relancer(des:int ,condition:Condition, n_condition:int, n_face:int) -> int:
         int: Le nouveau dès (ou l'ancien!)
     """
 
-    if eval_condition_map(des, condition, n_condition):
+    if _eval_condition(des, condition, n_condition):
         return randint(1,n_face-1)
-    else:
-        return des
+    return des
 
-def rajouter(lancer_des:Lancer_des, condition:Condition, n_condition:int, n_face:int):
+def rajouter(lancer_des: LancerDes, condition: Condition, n_condition: int, n_face: int):
+    """Rajoute un seul nouveau dès pour chaque dès respectant la condition"""
 
     def rajouter_si(des):
-        if eval_condition_map(des, condition, n_condition):
+        if _eval_condition(des, condition, n_condition):
             lancer_des.append(randint(1,n_face))
         return des
-    
-    return map(rajouter_si, lancer_des)
-    
-def appliquer_map(lancer_des:Lancer_des, action_map:str, condition_map:Condition, n_condition_map:int, n_face:int):
-    fn_condition = lambda des:eval_condition_map(des, condition_map, n_condition_map)
 
-    match condition_map:
-        case "!":
-            condition_map = "!="
-        case "=":
-            condition_map = "=="
-    
+    return map(rajouter_si, lancer_des)
+
+def appliquer_map(lancer_des:LancerDes, action_map:str,
+                  condition_map:Condition, n_condition_map:int, n_face:int):
+    """Applique Map sur un lancé de dès """
+    fn_condition = partial(_eval_condition, condition=condition_map, n_condition=n_condition_map)
+
     match action_map:
         case "r":
-            fn_condition = lambda des: relancer(des, condition_map, n_condition_map, n_face)
-
-            lancer_des = map(fn_condition, lancer_des)
+            lancer_des = map(
+                lambda des: relancer(des, condition_map, n_condition_map, n_face),
+                lancer_des)
 
         case "g":
             lancer_des = filter(fn_condition, lancer_des)
-            
+
         case "a":
             return rajouter(lancer_des, condition_map, n_condition_map, n_face)
 
@@ -179,55 +218,103 @@ def appliquer_map(lancer_des:Lancer_des, action_map:str, condition_map:Condition
 
     return list(lancer_des)
 
-def exec_commande(cmd:int, user_id=1):
-    "Prend en entré la commande (avec le point d'exclamation), et renvoie le message à afficher"
-    if cmd in ("!rank", "!levels"):
+def exec_commande(cmd: int, user_id: int=1) -> str:
+    "Prend en entré la commande (avec le point d'exclamation), l'execute, et renvoie le message à afficher"
+    seed(time())
+
+    if cmd in IGNORED_COMMANDS:
         return ""
-    
-    cmd = cmd[1:]
+
+    cmd: str = cmd[1:]
 
     try:
-        if match(RE_FORMAT_U, cmd):
-            result = exec_u(cmd)
+        if "u" in cmd:
+            result_message = exec_u(cmd)
 
-        elif match(RE_FORMAT_L, cmd):
-            result = exec_l(cmd)
-        
-        elif cmd[0] == "!":
+        elif "l" in cmd:
+            result_message = exec_l(cmd)
+
+        elif cmd == "h":
+            result_message = relancer_last_sr(user_id)
+
+        elif cmd == "sr":
+            if user_id in sr_users:
+                sr_users.remove(user_id)
+                result_message = "!SR Désactivé."
+            else:
+                sr_users.add(user_id)
+                result_message = "!SR Activé."
+            
+        elif cmd.startswith("!"):
             result = exec_cmd_macro(cmd, user_id)
-            if type(result) == tuple:
+
+            if type(result) is tuple:
                 print("Resultat : ", result[0])
                 somme, des = exec_lancer_des(result[0])
-                result = f'# {somme}\n{cmd} {des}'
+                result_message = f'# {somme}\n{cmd} {des}'
+            else:
+                result_message = result
+
+        elif cmd == "help":
+            result_message = "Documentation : https://github.com/Otomatyk/Dice-IO"
+
         else:
             cmd = cmd.lower()
             cmd = cmd.replace(" ", "" )
 
-            somme, des = exec_lancer_des(cmd)
-            result = f'# {somme}\n{cmd} {des}'
+            if user_id in sr_users:
+                result_message = exec_lancer_des_sr(cmd, user_id)
+
+                if type(result_message) is tuple:
+                    somme, des = result_message
+                    result_message = f'# {somme}\n{cmd} {des}'
+                elif type(result_message) is str:
+                    result_message = "# "+result_message
+                else:
+                    raise ValueError("Erreur fatale, type incorrecte pour le !sr")
+            else:
+                somme, des = exec_lancer_des(cmd)
+                result_message = f'# {somme}\n{cmd} {des}'
 
     except AssertionError as erreur:
-        result = 'Erreur : ' + erreur.args[0]
+        result_message = 'Erreur : ' + erreur.args[0]
 
     except EOFError as erreur:
         match erreur.args[0]:
             case "'NoneType' object has no attribute 'group'":
-                result = "Erreur indéterminée, possibles causes : \n\tLes divisions et multiplications ne sont pas prises en comptes"
-            
+                result_message = \
+                  "Erreur indéterminée, possibles causes : \n\tLes divisions et multiplications ne sont pas prises en comptes"
+
             case _:
-                result = 'Erreur indéterminée, cause : '+ erreur.args[0]
+                result_message = 'Erreur indéterminée, cause : '+ erreur.args[0]
+
     except Exception as erreur:
-        result = 'Erreur indéterminée, cause : '+ erreur.args[0]
-   
-    finally:
-        if len(result) >= MAX_MESSAGE_LEN:
-            return '```md\nMessage trop long```'
-        else:
-            return f'```md\n{result}```'
+        result_message = 'Erreur indéterminée, cause : '+ erreur.args[0]
+
+    if len(result_message) >= MAX_MESSAGE_LEN:
+        result_message = "Message trop long"
+
+    return f'```md\n{result_message}```'
 
 if __name__ == "__main__":
-    cmd = 1
     print("-*-*- DICE ENGINE -*-*-")
-    while cmd not in ("quit", "exit", "bye", "quitter"):
-        cmd = input("$ ")
-        print( exec_commande("!"+cmd) )
+
+    last_cmd: str = ""
+    QUIT_COMMAND = {"!quit", "!exit", "!quitter"}
+
+    while True:
+        INPUT_CMD = "!"+input("$ ")
+
+        if INPUT_CMD in QUIT_COMMAND:
+            break
+
+        if INPUT_CMD == "!":
+            INPUT_CMD = last_cmd
+            print(INPUT_CMD)
+
+        # Supprime le '''md\n'''
+        print( exec_commande(INPUT_CMD)[6:-3] )
+
+        last_cmd = INPUT_CMD
+
+    sys.exit()
